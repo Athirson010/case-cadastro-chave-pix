2746aca724e3c702528ac1ff7dd594401d421f69
athirson.candido <athirson.candido@bandtec.com.br>
2024-05-23 19:28:16 -0300

    test: 91%

--- src/test/java/io/github/athirson010/cadastro_chaves_pix/utils/validacoes/ValidacaoChaveTest.java
+++ src/test/java/io/github/athirson010/cadastro_chaves_pix/utils/validacoes/ValidacaoChaveTest.java
@@ -1,12 +1,15 @@
 package io.github.athirson010.cadastro_chaves_pix.utils.validacoes;
 
+import io.github.athirson010.cadastro_chaves_pix.domains.enums.TipoChaveEnum;
 import io.github.athirson010.cadastro_chaves_pix.domains.models.ChaveModel;
 import io.github.athirson010.cadastro_chaves_pix.exceptions.ValidacaoException;
 import io.github.athirson010.cadastro_chaves_pix.services.ChaveService;
+import io.github.athirson010.cadastro_chaves_pix.utils.validacoes.impl.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static io.github.athirson010.cadastro_chaves_pix.utils.validacoes.ValidacaoChave.resgatarTipoValidacao;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -31,5 +34,14 @@ class ValidacaoChaveTest {
         ValidacaoChave validacaoChave = new ValidacaoChave(chaveService);
         ValidacaoException exception = assertThrows(ValidacaoException.class, () -> validacaoChave.validarExistencia(valorChave));
         assertEquals("422 UNPROCESSABLE_ENTITY", exception.getMessage());
+    }
+
+    @Test
+    void testResgatarTipoValidacao() {
+        assertTrue(resgatarTipoValidacao(TipoChaveEnum.CELULAR) instanceof ChavePixValidacaoCelular);
+        assertTrue(resgatarTipoValidacao(TipoChaveEnum.EMAIL) instanceof ChavePixValidacaoEmail);
+        assertTrue(resgatarTipoValidacao(TipoChaveEnum.CPF) instanceof ChavePixValidacaoCPF);
+        assertTrue(resgatarTipoValidacao(TipoChaveEnum.CNPJ) instanceof ChavePixValidacaoCNPJ);
+        assertTrue(resgatarTipoValidacao(TipoChaveEnum.ALEATORIA) instanceof ChavePixValidacaoAleatoria);
     }
 }
