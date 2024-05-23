91ea9a9efd7caf0caf25e7c9ca0af4095af31dd8
athirson.candido <athirson.candido@bandtec.com.br>
2024-05-23 12:15:59 -0300

    add: sonar no app

---
+++ src/test/java/io/github/athirson010/cadastro_chaves_pix/business/ChavePixBusinessTest.java
@@ -0,0 +1,12 @@
+package io.github.athirson010.cadastro_chaves_pix.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChavePixBusinessTest {
+
+    @Test
+    void buscarPorId() {
+    }
+}
