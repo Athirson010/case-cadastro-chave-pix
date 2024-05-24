cfbb64e430f4d3a7694584229940622223590fb7
athirson.candido <athirson.candido@bandtec.com.br>
2024-05-23 22:32:11 -0300

    update: Timezone

--- src/main/java/io/github/athirson010/cadastro_chaves_pix/Application.java
+++ src/main/java/io/github/athirson010/cadastro_chaves_pix/Application.java
@@ -5,12 +5,15 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @OpenAPIDefinition(servers = {
         @Server(url = "/", description = "Default Server URL")
 })
 public class Application {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Brazil/East"));
         SpringApplication.run(Application.class, args);
     }
 }
