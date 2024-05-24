8bcd57bbebd2b5ef4fea4a324ee89d3bfda028be
athirson.candido <athirson.candido@bandtec.com.br>
2024-05-24 06:12:24 -0300

    fix: codesmells

--- src/main/java/io/github/athirson010/cadastro_chaves_pix/Application.java
+++ src/main/java/io/github/athirson010/cadastro_chaves_pix/Application.java
@@ -13,7 +13,6 @@ import java.util.TimeZone;
 })
 public class Application {
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("Brazil/East"));
         SpringApplication.run(Application.class, args);
     }
 }
